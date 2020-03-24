6ba0f731796709a15225d35bb8d289d0918ffdd8
Jose Saravia <josesaravia@Joses-MBP.fios-router.home>
2020-03-24 17:22:02 -0400

    Added auth and cros-cors

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router(require("./generate.js")());
+// const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 4000;
 
 // /!\ Bind the router db to the app
 server.db = router.db;
-
+// Enable pre-flight across-the-board
+server.options("*", cors());
 const rules = auth.rewriter({
   // Permission rules
   students: 660,
@@ -17,8 +20,17 @@ const rules = auth.rewriter({
 server.use(rules);
 server.use(auth);
 server.use(middlewares);
+// For CROSS-CORS POLICY STUFF
+/*Equivalent to:
+{
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}
+*/
+server.use(cors());
 server.use(router);
-
 server.listen(port, () => {
   console.log(`
   
